e6301028948fd0bd91b821471f139a6363a3632e
Kim-Onesmus <kimones1122@gmail.com>
2024-02-29 14:33:01 +0300

    updates

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import auth, User
 from django.contrib import messages
 from .models import User
