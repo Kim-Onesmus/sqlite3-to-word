8460da749ed0bbccd5765e2d9ae1fa2c78190a2b
Kim-Onesmus <kimones1122@gmail.com>
2024-02-29 15:15:02 +0300

    updates

--- app/models.py
+++ app/models.py
@@ -2,6 +2,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+category_choice = [
+    ('sports','sports'),
+    ('education','education'),
+    ('politics','politics'),
+    ('administrative','administrative'),
+    ('religious','religious'),
+]
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
@@ -13,3 +21,13 @@ class User(AbstractUser):
     
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+
+class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=200, choices=category_choice)
+    tittle = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    text = models.TextField(max_length=1000)
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
