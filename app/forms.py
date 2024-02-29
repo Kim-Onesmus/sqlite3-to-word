2bdc9fb7f863c67c83cb7ba71cdfa2a0ac52e95e
Kim-Onesmus <kimones1122@gmail.com>
2024-02-29 15:26:14 +0300

    updates

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,17 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
-from .models import User
+from .models import User, News
 from django import forms
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
         fields = '__all__'
