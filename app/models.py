f602b7ff816a63593a98195fa8dc1bbee09418be
Kim-K <kim-k@localhost.localdomain>
2024-02-29 13:55:11 +0300

    updates

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100)
     mobile_no = PhoneNumberField(blank=False)
     date_updated = models.DateTimeField(auto_now=True)
 
